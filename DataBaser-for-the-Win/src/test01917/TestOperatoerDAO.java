ab7da8dc8b4b37be12fa343d2cad87f39f256a7d
Niko <nikooftheworld@gmail.com>
2015-04-21 10:21:21 +0200

    Tilføjet test og JUnit libery

---
+++ DataBaser-for-the-Win/src/test01917/TestOperatoerDAO.java
@@ -0,0 +1,14 @@
+package test01917;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestOperatoerDAO {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

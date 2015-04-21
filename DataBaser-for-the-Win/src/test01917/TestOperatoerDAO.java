25a958d1b94309df83659a93b3d4435fb9b35107
Niko <nikooftheworld@gmail.com>
2015-04-21 10:31:49 +0200

    Oprettet test til implementeringer

--- DataBaser-for-the-Win/src/test01917/TestOperatoerDAO.java
+++ DataBaser-for-the-Win/src/test01917/TestOperatoerDAO.java
@@ -5,9 +5,24 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestOperatoerDAO {
+	
+	@Test
+	public void getOperatoer() {
+		fail("Not yet implemented");
+	}
 
 	@Test
-	public void test() {
+	public void createOperatoer() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void updateOperatoer() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void getOperatoerList() {
 		fail("Not yet implemented");
 	}
 

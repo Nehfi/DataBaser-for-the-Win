867323954cdd760ae55da3227a256680d1ac7edb
eggman1979 <kim.d.rasmussen@live.dk>
2015-04-21 10:30:25 +0200

    oprettet tests

---
+++ DataBaser-for-the-Win/src/test01917/TestRaavareDAO.java
@@ -0,0 +1,27 @@
+package test01917;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import daointerfaces01917.DALException;
+import dto01917.RaavareDTO;
+
+public class TestRaavareDAO {
+
+//	RaavareDTO getRaavare(int raavareId) throws DALException;
+//	List<RaavareDTO> getRaavareList() throws DALException;
+//	void createRaavare(RaavareDTO raavare) throws DALException;
+//	void updateRaavare(RaavareDTO raavare) throws DALException;
+	
+	
+	
+	@Test
+	public void testGetRaavare() {
+		
+		fail("Not yet implemented");
+	}
+
+}

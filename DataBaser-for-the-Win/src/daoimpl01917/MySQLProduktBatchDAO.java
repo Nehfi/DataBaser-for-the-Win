c3e844f5a1a416d164677f26e525766dd5979f11
Niko <nikooftheworld@gmail.com>
2015-04-21 10:03:52 +0200

    Oprettet implementerings filer til DAO interfaces

---
+++ DataBaser-for-the-Win/src/daoimpl01917/MySQLProduktBatchDAO.java
@@ -0,0 +1,37 @@
+package daoimpl01917;
+
+import java.util.List;
+
+import daointerfaces01917.DALException;
+import daointerfaces01917.ProduktBatchDAO;
+import dto01917.ProduktBatchDTO;
+
+public class MySQLProduktBatchDAO implements ProduktBatchDAO {
+
+	@Override
+	public ProduktBatchDTO getProduktBatch(int pbId) throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProduktBatchDTO> getProduktBatchList() throws DALException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void createProduktBatch(ProduktBatchDTO produktbatch)
+			throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateProduktBatch(ProduktBatchDTO produktbatch)
+			throws DALException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

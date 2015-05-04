a3f8adf59b5a30536d78a8676f08c18e9492db1e
Niko <nikooftheworld@gmail.com>
2015-05-04 17:49:13 +0200

    Lavet test til at vise at implementeringen af Raavare virker

--- DataBaser-for-the-Win/src/test01917/TestRaavareDAO.java
+++ DataBaser-for-the-Win/src/test01917/TestRaavareDAO.java
@@ -2,8 +2,14 @@ package test01917;
 
 import static org.junit.Assert.*;
 import daoimpl01917.MySQLRaavareDAO;
+
+import java.sql.SQLException;
 import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import connector01917.Connector;
 import daointerfaces01917.DALException;
 import daointerfaces01917.RaavareDAO;
 import dto01917.RaavareDTO;
@@ -11,10 +17,20 @@ import dto01917.RaavareDTO;
 public class TestRaavareDAO {
 
 	RaavareDAO rd = new MySQLRaavareDAO();
-
+	
+	@Before
+	public void connect()
+	{
+		try {
+			new Connector();
+		} catch (Exception e) {
+		
+		}
+	}
 
 	@Test
 	public void testGetRaavare() throws DALException {
+	
 
 		RaavareDTO testVare = null;
 		List<RaavareDTO> raavareList = rd.getRaavareList();
@@ -24,37 +40,40 @@ public class TestRaavareDAO {
 		RaavareDTO actual = testVare;
 		RaavareDTO expected = raavareList.get(0);
 
-		assertEquals(expected, actual);
+		boolean elementsAreTheSame = true;
+		
+		if (actual.getRaavareId() 	!= expected.getRaavareId()) 	   elementsAreTheSame = false;
+		if (!actual.getRaavareNavn().equals(expected.getRaavareNavn())) elementsAreTheSame = false;
+		if (!actual.getLeverandoer().equals(expected.getLeverandoer())) elementsAreTheSame = false;
 
+		assertTrue(elementsAreTheSame);
 
 	}
 
 	@Test
 	public void testGetRaavareList() throws DALException{
-		boolean actual = false;
-		int expectedSize = rd.getRaavareList().size();
-		if(expectedSize > 0){
-		}
 
-		boolean expected = true;
-
-		assertEquals(expected, actual);
+		List<RaavareDTO> list = rd.getRaavareList();
+		
+		assertTrue(list.size()>1);
 	} 
+	
 	@Test
-	public void TestCreateRaavare(){
-		int expected=  0, actual = 0;
-		try {
-			expected = rd.getRaavareList().size() + 1;
-			rd.createRaavare(new RaavareDTO(100, "Banan", "taberstedet"));
-			actual =  rd.getRaavareList().size();
-		} catch (DALException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void TestCreateRaavare() throws DALException{
+	
+		List<RaavareDTO> list = rd.getRaavareList();
+		int currentHighestID  = list.get(list.size()-1).getRaavareId();
+		
+		int expected = rd.getRaavareList().size()+1;
+		rd.createRaavare(new RaavareDTO(currentHighestID+1, "Banan", "stedet"));
+		int actual =  rd.getRaavareList().size();
+		
+		
 
 		assertEquals(expected, actual);
 
 	}
+	
 	@Test
 	public void testUpdateRaaVare(){
 		RaavareDTO dto = null;

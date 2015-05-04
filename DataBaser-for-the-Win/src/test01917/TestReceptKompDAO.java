6efe849e917b24dff3e3db120d04467baabeb2a4
eggman1979 <kim.d.rasmussen@live.dk>
2015-05-05 01:42:03 +0200

    Har lavet tests i TestreceptKomp

--- DataBaser-for-the-Win/src/test01917/TestReceptKompDAO.java
+++ DataBaser-for-the-Win/src/test01917/TestReceptKompDAO.java
@@ -2,18 +2,92 @@ package test01917;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import connector01917.Connector;
+import daoimpl01917.MySQLReceptKompDAO;
+import daointerfaces01917.DALException;
+import dto01917.RaavareDTO;
+import dto01917.ReceptKompDTO;
 
 public class TestReceptKompDAO {
 
-	@After
-	public void tearDown() throws Exception {
+	
+	//	void updateReceptKomp(ReceptKompDTO receptkomponent) throws DALException;
+
+	MySQLReceptKompDAO rk = new MySQLReceptKompDAO();
+
+
+	@Before
+	public void Connect() {
+		try {
+			new Connector();
+		}catch (Exception e){
+		}
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testgetReceptKomp() throws DALException{
+
+		List<ReceptKompDTO> list = rk.getReceptKompList();
+		ReceptKompDTO expected = list.get(0);
+		int key1 = expected.getRaavareId();
+		int key2 = expected.getReceptId();
+
+		ReceptKompDTO  actual = rk.getReceptKomp(key1, key2);
+
+		assertEquals(actual, expected);
 	}
 
+	@Test
+	public void testGetReceptKompList() throws DALException{
+		boolean listMoreThanZero = false;
+
+		if(rk.getReceptKompList().size() > 0){
+			listMoreThanZero = true;
+		}
+		assertTrue(listMoreThanZero);
+	}
+
+	@Test
+	public void getReceptKompListWithReceptID() throws DALException{
+		int receptID = rk.getReceptKompList().get(0).getReceptId();
+		
+		boolean listMoreThanZero = false;
+
+		if(rk.getReceptKompList(receptID).size() > 0){
+			listMoreThanZero = true;
+		}
+		assertTrue(listMoreThanZero);
+	}
+	
+	@Test
+	public void testCreateReceptKomp() throws DALException{
+		int expected = rk.getReceptKompList().size() + 1;
+		 new ReceptKompDTO(1, 2, 1, 2);
+		rk.createReceptKomp( new ReceptKompDTO(1, 2, 1, 2));
+		int actual = rk.getReceptKompList().size();
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testUpdateReceptKomp() throws DALException{
+		
+		
+		ReceptKompDTO one = rk.getReceptKompList().get(0);
+		one.setTolerance(0.01);
+		rk.updateReceptKomp(one);
+		double expected = 3.0;
+		one.setTolerance(expected);
+		rk.updateReceptKomp(one);
+		
+		double actual = rk.getReceptKompList().get(0).getTolerance();
+		//Der kan opstå fejl hvis den afrunder decimalerne forkert
+		assertTrue(actual == expected);
+	}
 }

89c347619015153e3c46dd205274fbe4550714ec
Onepiece <Onepiece@192.168.1.7>
2015-05-04 21:05:04 +0200

    RaavareBatch.. sider lidt fast

--- DataBaser-for-the-Win/src/test01917/TestRaavareBatchDAO.java
+++ DataBaser-for-the-Win/src/test01917/TestRaavareBatchDAO.java
@@ -2,47 +2,72 @@ package test01917;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Test;
 
+import connector01917.Connector;
+import daoimpl01917.MySQLRaavareBatchDAO;
+import daointerfaces01917.DALException;
+import daointerfaces01917.RaavareBatchDAO;
+import dto01917.RaavareBatchDTO;
+
 public class TestRaavareBatchDAO {
+
+	private RaavareBatchDAO dao = new MySQLRaavareBatchDAO();
 
 	
 	@Test
-	public void getRaavareBatch() {
+	public void getRaavareBatch() throws DALException, InstantiationException, IllegalAccessException, ClassNotFoundException, SQLException {
 		
-		// Brug getRaavareBatchList til at hente et element ud.
-		// brug derefter getRaavareBatch til at hente et element der deler værdier med det element du har hentet ud med list metoden
-		// Sammenlign dem!
-		fail("Not yet implemented");
+		new Connector();
+		
+		
+		RaavareBatchDAO rbDAO= null;
+		
+		@SuppressWarnings("unchecked")
+		List<RaavareBatchDTO> rbDAOList = (List<RaavareBatchDTO>) dao.getRaavareBatch(1);
+		@SuppressWarnings("unchecked")
+		int validId = ((List<RaavareBatchDTO>) dao.getRaavareBatch(1)).get(1).getRbId();
+		
+		RaavareBatchDTO actual = dao.getRaavareBatch(validId);
+		RaavareBatchDTO expected = rbDAOList.get(0);
+		
+		assertEquals(expected, actual);
+		
+
 	}
 	
 	@Test
-	public void getRaavareBatchList() {
-		// Check at du kan få en liste tilbage der indeholder mere end 1 element.
-		fail("Not yet implemented");
+	public void getRaavareBatchList() throws DALException {
+		
+		RaavareBatchDAO rbDAO= null;
+		
+		List<RaavareBatchDTO> rbDAOList = dao.getRaavareBatchList();
+		int validId = dao.getRaavareBatchList().get(0).getRbId();
+		
+		RaavareBatchDTO actual = dao.getRaavareBatch(validId);
+		RaavareBatchDTO expected = rbDAOList.get(0);
+		
+		assertEquals(expected, actual);
+		
 	}
 	
 	@Test
 	public void getRaavareBatchListWithraavareId() {
-		// Gør som i første test
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void createRaavareBatch() {
-		// Indsæt et raavareBatchDTO object, og tjek at databasen er blevet et element størrer end før du gjorde det.
-		// Måske hvis du har overskud, se om det er netop dette nye element den er blevet forlænget med.
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void updateRaavareBatch() {
-		// Find et element, ændre noget i det. - update det.
-		// Hiv elementet udigen med getRaavareBatch, og se om ændringerne blev gemt rigtig
 		fail("Not yet implemented");
 	}
-	
-	// God arbejdslyst!
 
 }

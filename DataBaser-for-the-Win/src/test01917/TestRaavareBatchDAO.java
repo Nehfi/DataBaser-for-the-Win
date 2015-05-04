28d5c3d76e576f7039091be45275aecdd9d358d5
Niko <nikooftheworld@gmail.com>
2015-05-04 15:32:58 +0200

    Tilføjet kommentare

--- DataBaser-for-the-Win/src/test01917/TestRaavareBatchDAO.java
+++ DataBaser-for-the-Win/src/test01917/TestRaavareBatchDAO.java
@@ -10,27 +10,39 @@ public class TestRaavareBatchDAO {
 	
 	@Test
 	public void getRaavareBatch() {
+		
+		// Brug getRaavareBatchList til at hente et element ud.
+		// brug derefter getRaavareBatch til at hente et element der deler værdier med det element du har hentet ud med list metoden
+		// Sammenlign dem!
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void getRaavareBatchList() {
+		// Check at du kan få en liste tilbage der indeholder mere end 1 element.
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void getRaavareBatchListWithraavareId() {
+		// Gør som i første test
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void createRaavareBatch() {
+		// Indsæt et raavareBatchDTO object, og tjek at databasen er blevet et element størrer end før du gjorde det.
+		// Måske hvis du har overskud, se om det er netop dette nye element den er blevet forlænget med.
 		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void updateRaavareBatch() {
+		// Find et element, ændre noget i det. - update det.
+		// Hiv elementet udigen med getRaavareBatch, og se om ændringerne blev gemt rigtig
 		fail("Not yet implemented");
 	}
+	
+	// God arbejdslyst!
 
 }

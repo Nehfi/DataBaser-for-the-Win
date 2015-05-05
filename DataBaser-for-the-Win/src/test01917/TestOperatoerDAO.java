2e6ba9815299dbe24ddb74506cb74c5f1b5a86c2
JacobLA <jacob_jau@hotmail.com>
2015-05-05 10:05:37 +0200

    Test: Operatoer/ProduktBatch: done.

--- DataBaser-for-the-Win/src/test01917/TestOperatoerDAO.java
+++ DataBaser-for-the-Win/src/test01917/TestOperatoerDAO.java
@@ -3,31 +3,88 @@ package test01917;
 import static org.junit.Assert.*;
 
 import java.sql.Connection;
+import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import connector01917.Connector;
+import connector01917.*;
+import daoimpl01917.*;
+import daointerfaces01917.*;
+import dto01917.*;
+
 
 public class TestOperatoerDAO {
 	
-	@Test
-	public void getOperatoer() {
+	OperatoerDAO opDao  = new MySQLOperatoerDAO();
+	
+	@Before
+	public void connect()
+	{
+		try {
+			new Connector();
+		} catch (Exception e) {
 		
+		}
+	}
+	
+	@Test
+	public void TestGetOperatoer() throws DALException {
+		OperatoerDTO testOperatoer = null;
+		List<OperatoerDTO> operatoerList = opDao.getOperatoerList();
+		int ID = operatoerList.get(0).getOprId();
+		testOperatoer = opDao.getOperatoer(ID);
+
+		OperatoerDTO actual = testOperatoer;
+		OperatoerDTO expected = operatoerList.get(0);
+
+		boolean theSame = true;
+		
+		if (actual.getOprId() 	!= expected.getOprId()) 	   		theSame = false;
+		if (!actual.getOprNavn().equals(expected.getOprNavn())) 	theSame = false;
+		if (!actual.getCpr().equals(expected.getCpr())) 			theSame = false;
+		if (!actual.getIni().equals(expected.getIni())) 			theSame = false;
+		if (!actual.getPassword().equals(expected.getPassword())) 	theSame = false;
+
+		assertTrue(theSame);
 	}
 
 	@Test
-	public void createOperatoer() {
-		fail("Not yet implemented");
+	public void getOperatoerList() throws DALException {
+	
+		List<OperatoerDTO> list = opDao.getOperatoerList();
+		
+		assertTrue(list.size()>1);
 	}
 	
 	@Test
-	public void updateOperatoer() {
-		fail("Not yet implemented");
+	public void createOperatoer() throws DALException{
+		List<OperatoerDTO> list = opDao.getOperatoerList();
+		int currentHighestID  = list.get(list.size()-1).getOprId();
+		
+		int expected = opDao.getOperatoerList().size()+1;
+		opDao.createOperatoer(new OperatoerDTO(currentHighestID+1, "Jens Jensen", "JJ", "090591-2929", "Ss123Ss"));
+		int actual =  opDao.getOperatoerList().size();
+		
+		
+
+		assertEquals(expected, actual);
 	}
 	
 	@Test
-	public void getOperatoerList() {
-		fail("Not yet implemented");
+	public void updateOperatoer() throws DALException {
+		OperatoerDTO opDto = null;
+		String expected = "Jens Jensen";
+		try {
+			opDto = opDao.getOperatoerList().get(0);
+			opDto.setOprNavn(expected);
+			opDao.updateOperatoer(opDto);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String actual = opDto.getOprNavn();
+		assertEquals(expected, actual);
 	}
 
 }
--- DataBaser-for-the-Win/src/test01917/TestProduktBatchDAO.java
+++ DataBaser-for-the-Win/src/test01917/TestProduktBatchDAO.java
@@ -2,46 +2,88 @@ package test01917;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import connector01917.Connector;
 import daoimpl01917.MySQLProduktBatchDAO;
 import daointerfaces01917.DALException;
 import daointerfaces01917.ProduktBatchDAO;
 import dto01917.ProduktBatchDTO;
+import dto01917.RaavareDTO;
 
 public class TestProduktBatchDAO {
 	
-	ProduktBatchDAO impl = new MySQLProduktBatchDAO();
+	ProduktBatchDAO pbDao = new MySQLProduktBatchDAO();
+	
+	@Before
+	public void connect()
+	{
+		try {
+			new Connector();
+		} catch (Exception e) {
+		
+		}
+	}
 
 	@Test
-	public void getProduktBatch() throws DALException {
+	public void testGetProduktBatch() throws DALException
+	{
+		ProduktBatchDTO testBatch = null;
+		List<ProduktBatchDTO> pbList = pbDao.getProduktBatchList();
+		int validId = pbList.get(0).getPbId();
+		testBatch = pbDao.getProduktBatch(validId);
 		
-		ProduktBatchDTO pbDTO = null;
-		List<ProduktBatchDTO> pbDTOList = impl.getProduktBatchList();
-		int validId = impl.getProduktBatchList().get(0).getPbId();
+		ProduktBatchDTO actual = testBatch;
+		ProduktBatchDTO expected = pbList.get(0);
 		
-		ProduktBatchDTO actual = impl.getProduktBatch(validId);
-		ProduktBatchDTO expected = pbDTOList.get(0);
+		boolean theSame = true;
 		
+		if (actual.getPbId() 	!= expected.getPbId()) 	   theSame = false;
+		if (actual.getReceptId() != expected.getReceptId()) theSame = false;
+		if (actual.getStatus() != expected.getStatus()) theSame = false;
+
+		assertTrue(theSame);
+		
+	}
+	
+	@Test
+	public void testGetProduktBatchList() throws DALException {
+		
+		List<ProduktBatchDTO> pbList = pbDao.getProduktBatchList();
+		
+		assertTrue(pbList.size()>1);
+	}
+	
+	@Test
+	public void testCreateProduktBatch() throws DALException {
+		List<ProduktBatchDTO> pbList = pbDao.getProduktBatchList();
+		int currentHighestId  = pbList.get(pbList.size()-1).getPbId();
+	
+		int expected = pbDao.getProduktBatchList().size()+1;
+		pbDao.createProduktBatch(new ProduktBatchDTO(currentHighestId+1, 1, 1));
+		int actual = pbDao.getProduktBatchList().size();
+	
 		assertEquals(expected, actual);
-		
 	}
 	
 	@Test
-	public void getProduktBatchList() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void createProduktBatch() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void updateProduktBatch() {
-		fail("Not yet implemented");
+	public void testUpdateProduktBatch() {
+		ProduktBatchDTO pbList = null;
+		int expected = 1;
+		try {
+			pbList = pbDao.getProduktBatchList().get(0);
+			pbList.setReceptId(expected);
+			pbDao.updateProduktBatch(pbList);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int actual = pbList.getReceptId();
+		assertEquals(expected, actual);
 	}
 
 }
